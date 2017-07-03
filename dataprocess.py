37257f14e9062477da38c02e066bd21d4ce20c7b
Jackie Zhang <viperking@163.com>
2017-07-03 23:56:47 +0800

    added tensorflow code

--- dataprocess.py
+++ dataprocess.py
@@ -4,219 +4,219 @@ import pandas as pd
 import numpy as np
 import random as rnd
 
-# visualization
-import seaborn as sns
-import matplotlib.pyplot as plt
-#%matplotlib inline
-
-# machine learning
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC, LinearSVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.linear_model import Perceptron
-from sklearn.linear_model import SGDClassifier
-from sklearn.tree import DecisionTreeClassifier
-
-#load data
-train_df = pd.read_csv('./train.csv')
-test_df = pd.read_csv('./test.csv')
-combine = [train_df, test_df]
-
-# print data correlation analysis
-#print(train_df.head())
-#display info
-train_df.info()
-print('_'*40)
-test_df.info()
-print('_'*40)
-
-#print(train_df.describe(include=['O']))
-print(train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False))
-print('_'*40)
-print(train_df[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False))
-
-#histogram
-#g = sns.FacetGrid(train_df, col='Survived')
-#g.map(plt.hist, 'Age', bins=20)
-#plt.show()
-
-#grid = sns.FacetGrid(train_df, col='Survived', row='Pclass', size=2.2, aspect=1.6)
-#grid.map(plt.hist, 'Age', alpha=.5, bins=20)
-#grid.add_legend();
-#plt.show()
-
-#grid = sns.FacetGrid(train_df, row='Embarked', size=2.2, aspect=1.6)
-#grid.map(sns.pointplot, 'Pclass', 'Survived', 'Sex', palette='deep')
-#grid.add_legend()
-#plt.show()
-
-#grid = sns.FacetGrid(train_df, row='Embarked', col='Survived', size=2.2, aspect=1.6)
-#grid.map(sns.barplot, 'Sex', 'Fare', alpha=.5, ci=None)
-#grid.add_legend()
-#plt.show()
 
 
-#drop unused columns
-print("Before", train_df.shape, test_df.shape, combine[0].shape, combine[1].shape)
-train_df = train_df.drop(['Ticket', 'Cabin'], axis=1)
-test_df = test_df.drop(['Ticket', 'Cabin'], axis=1)
-combine = [train_df, test_df]
-print("After", train_df.shape, test_df.shape, combine[0].shape, combine[1].shape)
+def loaddata():
+    #load data
+    train_df = pd.read_csv('./train.csv')
+    test_df = pd.read_csv('./test.csv')
+    combine = [train_df, test_df]
 
-#title extract
-for dataset in combine:
-    dataset['Title'] = dataset.Name.str.extract('([a-zA-Z]+)\.', expand=False)
+    # print data correlation analysis
+    #print(train_df.head())
+    #display info
+    train_df.info()
+    print('_'*40)
+    test_df.info()
+    print('_'*40)
 
-print(pd.crosstab(train_df['Title'], train_df['Sex']))
+    #print(train_df.describe(include=['O']))
+    print(train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+    print('_'*40)
+    print(train_df[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False))
 
-for dataset in combine:
-    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess', 'Capt', 'Col',
-                                                 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+    #histogram
+    #g = sns.FacetGrid(train_df, col='Survived')
+    #g.map(plt.hist, 'Age', bins=20)
+    #plt.show()
 
-    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
-    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
-    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+    #grid = sns.FacetGrid(train_df, col='Survived', row='Pclass', size=2.2, aspect=1.6)
+    #grid.map(plt.hist, 'Age', alpha=.5, bins=20)
+    #grid.add_legend();
+    #plt.show()
 
-print(train_df[['Title', 'Survived']].groupby(['Title'], as_index=False).mean())
+    #grid = sns.FacetGrid(train_df, row='Embarked', size=2.2, aspect=1.6)
+    #grid.map(sns.pointplot, 'Pclass', 'Survived', 'Sex', palette='deep')
+    #grid.add_legend()
+    #plt.show()
 
-#title mapping
-title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
-for dataset in combine:
-    dataset['Title'] = dataset['Title'].map(title_mapping)
-    dataset['Title'] = dataset['Title'].fillna(0)
+    #grid = sns.FacetGrid(train_df, row='Embarked', col='Survived', size=2.2, aspect=1.6)
+    #grid.map(sns.barplot, 'Sex', 'Fare', alpha=.5, ci=None)
+    #grid.add_legend()
+    #plt.show()
 
-print(train_df.head())
 
-print('_'*40)
+    #drop unused columns
+    print("Before", train_df.shape, test_df.shape, combine[0].shape, combine[1].shape)
+    train_df = train_df.drop(['Ticket', 'Cabin'], axis=1)
+    test_df = test_df.drop(['Ticket', 'Cabin'], axis=1)
+    combine = [train_df, test_df]
+    print("After", train_df.shape, test_df.shape, combine[0].shape, combine[1].shape)
 
-#drop unused columns
-train_df = train_df.drop(['Name', 'PassengerId'], axis=1)
-test_df = test_df.drop(['Name'], axis=1)
-combine = [train_df, test_df]
-print(train_df.describe())
+    #title extract
+    for dataset in combine:
+        dataset['Title'] = dataset.Name.str.extract('([a-zA-Z]+)\.', expand=False)
 
-print('_'*40)
+    print(pd.crosstab(train_df['Title'], train_df['Sex']))
 
-#converting to numeric value
-for dataset in combine:
-    dataset['Sex'] = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+    for dataset in combine:
+        dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess', 'Capt', 'Col',
+                                                     'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
 
-print(train_df.head())
+        dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+        dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+        dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
 
-#fill null value
+    print(train_df[['Title', 'Survived']].groupby(['Title'], as_index=False).mean())
 
-guess_ages = np.zeros((2,3))
-print(guess_ages)
+    #title mapping
+    title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+    for dataset in combine:
+        dataset['Title'] = dataset['Title'].map(title_mapping)
+        dataset['Title'] = dataset['Title'].fillna(0)
 
-#Now we iterate over Sex (0 or 1) and Pclass (1, 2, 3) to calculate guessed values of Age for the six combinations.
-print('_'*40)
+    print(train_df.head())
 
-for dataset in combine:
-    for i in range(0, 2):
-        for j in range(0, 3):
-            guess_df = dataset[(dataset['Sex'] == i) & \
-                               (dataset['Pclass'] == j + 1)]['Age'].dropna()
+    print('_'*40)
 
-            # age_mean = guess_df.mean()
-            # age_std = guess_df.std()
-            # age_guess = rnd.uniform(age_mean - age_std, age_mean + age_std)
+    #drop unused columns
+    train_df = train_df.drop(['Name', 'PassengerId'], axis=1)
+    test_df = test_df.drop(['Name'], axis=1)
+    combine = [train_df, test_df]
+    print(train_df.describe())
 
-            age_guess = guess_df.median()
+    print('_'*40)
 
-            # Convert random age float to nearest .5 age
-            guess_ages[i, j] = int(age_guess / 0.5 + 0.5) * 0.5
+    #converting to numeric value
+    for dataset in combine:
+        dataset['Sex'] = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
 
-    for i in range(0, 2):
-        for j in range(0, 3):
-            dataset.loc[(dataset.Age.isnull()) & (dataset.Sex == i) & (dataset.Pclass == j + 1), \
-                        'Age'] = guess_ages[i, j]
+    print(train_df.head())
 
-    dataset['Age'] = dataset['Age'].astype(int)
+    #fill null value
 
-print(train_df.head())
+    guess_ages = np.zeros((2,3))
+    print(guess_ages)
 
-print('_'*40)
+    #Now we iterate over Sex (0 or 1) and Pclass (1, 2, 3) to calculate guessed values of Age for the six combinations.
+    print('_'*40)
 
-#Age banding
-train_df['AgeBand'] = pd.cut(train_df['Age'], 5)
-print(train_df[['AgeBand', 'Survived']].groupby(['AgeBand'], as_index=False).mean().sort_values(by='AgeBand', ascending=True))
+    for dataset in combine:
+        for i in range(0, 2):
+            for j in range(0, 3):
+                guess_df = dataset[(dataset['Sex'] == i) & \
+                                   (dataset['Pclass'] == j + 1)]['Age'].dropna()
 
-#Age banding
-for dataset in combine:
-    dataset.loc[ dataset['Age'] <= 16, 'Age'] = 0
-    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
-    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
-    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
-    dataset.loc[ dataset['Age'] > 64, 'Age'] = 4
-print(train_df.head())
+                # age_mean = guess_df.mean()
+                # age_std = guess_df.std()
+                # age_guess = rnd.uniform(age_mean - age_std, age_mean + age_std)
 
-print('_'*40)
+                age_guess = guess_df.median()
 
-#remove ageband
-train_df = train_df.drop(['AgeBand'], axis=1)
-combine = [train_df, test_df]
-print(train_df.head())
+                # Convert random age float to nearest .5 age
+                guess_ages[i, j] = int(age_guess / 0.5 + 0.5) * 0.5
 
-#create new feature:
-for dataset in combine:
-    dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+        for i in range(0, 2):
+            for j in range(0, 3):
+                dataset.loc[(dataset.Age.isnull()) & (dataset.Sex == i) & (dataset.Pclass == j + 1), \
+                            'Age'] = guess_ages[i, j]
 
-for dataset in combine:
-    dataset['IsAlone'] = 0
-    dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
+        dataset['Age'] = dataset['Age'].astype(int)
 
-for dataset in combine:
-    dataset['Age*Class'] = dataset.Age * dataset.Pclass
+    print(train_df.head())
 
-print('_'*40)
+    print('_'*40)
 
-#Let us drop Parch, SibSp, and FamilySize features in favor of IsAlone.
-train_df = train_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
-test_df = test_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
-combine = [train_df, test_df]
+    #Age banding
+    train_df['AgeBand'] = pd.cut(train_df['Age'], 5)
+    print(train_df[['AgeBand', 'Survived']].groupby(['AgeBand'], as_index=False).mean().sort_values(by='AgeBand', ascending=True))
 
-#Our training dataset has two missing values. We simply fill these with the most common occurance.
+    #Age banding
+    for dataset in combine:
+        dataset.loc[ dataset['Age'] <= 16, 'Age'] = 0
+        dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+        dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+        dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+        dataset.loc[ dataset['Age'] > 64, 'Age'] = 4
+    print(train_df.head())
 
-freq_port = train_df.Embarked.dropna().mode()[0]
-for dataset in combine:
-    dataset['Embarked'] = dataset['Embarked'].fillna(freq_port)
+    print('_'*40)
 
-#fill na values
-for dataset in combine:
-    dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+    #remove ageband
+    train_df = train_df.drop(['AgeBand'], axis=1)
+    combine = [train_df, test_df]
+    print(train_df.head())
 
-for dataset in combine:
-    dataset['Fare'].fillna(dataset['Fare'].dropna().median(), inplace=True)
+    #create new feature:
+    for dataset in combine:
+        dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
 
-train_df['FareBand'] = pd.qcut(train_df['Fare'], 4)
-print(train_df[['FareBand', 'Survived']].groupby(['FareBand'], as_index=False).mean().sort_values(by='FareBand', ascending=True))
+    for dataset in combine:
+        dataset['IsAlone'] = 0
+        dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
 
-print('_'*40)
+    for dataset in combine:
+        dataset['Age*Class'] = dataset.Age * dataset.Pclass
 
-for dataset in combine:
-    dataset.loc[dataset['Fare'] <= 7.91, 'Fare'] = 0
-    dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
-    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare'] = 2
-    dataset.loc[dataset['Fare'] > 31, 'Fare'] = 3
-    dataset['Fare'] = dataset['Fare'].astype(int)
+    print('_'*40)
 
-train_df = train_df.drop(['FareBand'], axis=1)
-combine = [train_df, test_df]
+    #Let us drop Parch, SibSp, and FamilySize features in favor of IsAlone.
+    train_df = train_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
+    test_df = test_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
+    combine = [train_df, test_df]
 
-#finished feature processing
-print(train_df.head())
+    #Our training dataset has two missing values. We simply fill these with the most common occurance.
 
-print('_'*40)
+    freq_port = train_df.Embarked.dropna().mode()[0]
+    for dataset in combine:
+        dataset['Embarked'] = dataset['Embarked'].fillna(freq_port)
 
-#convert to numpy array
+    #fill na values
+    for dataset in combine:
+        dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
 
-result = train_df.values
-print(result)
+    for dataset in combine:
+        dataset['Fare'].fillna(dataset['Fare'].dropna().median(), inplace=True)
 
-#convert to excel
-train_df.to_excel('foo.xlsx', sheet_name='Sheet1')
+    train_df['FareBand'] = pd.qcut(train_df['Fare'], 4)
+    print(train_df[['FareBand', 'Survived']].groupby(['FareBand'], as_index=False).mean().sort_values(by='FareBand', ascending=True))
 
-#breakpoint into ipython
-#ipdb.set_trace()
+    print('_'*40)
+
+    for dataset in combine:
+        dataset.loc[dataset['Fare'] <= 7.91, 'Fare'] = 0
+        dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+        dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare'] = 2
+        dataset.loc[dataset['Fare'] > 31, 'Fare'] = 3
+        dataset['Fare'] = dataset['Fare'].astype(int)
+
+    train_df = train_df.drop(['FareBand'], axis=1)
+    combine = [train_df, test_df]
+
+    #finished feature processing
+    print(train_df.head())
+
+    print('_'*40)
+
+    #convert to numpy array
+
+    result = train_df.values
+    print(result)
+
+    #convert to excel
+    #train_df.to_excel('foo.xlsx', sheet_name='Sheet1')
+
+    #breakpoint into ipython
+    #ipdb.set_trace()
+
+    print('_'*40)
+
+    #feature normization
+
+    for index in train_df.columns[1:]:
+        train_df[index] = (train_df[index] - train_df[index].mean()) / train_df[index].max()
+
+    for index in test_df.columns:
+        test_df[index] = (test_df[index] - test_df[index].mean()) / test_df[index].max()
+
+    return [train_df, test_df]
---
+++ train.py
@@ -0,0 +1,84 @@
+# visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+# machine learning
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+import tensorflow as tf
+import numpy as np
+
+from dataprocess import loaddata
+
+train_df, test_df = loaddata()
+
+x_train = train_df.drop('Survived', axis=1)
+y_train = train_df['Survived']
+
+x_test = test_df.drop('PassengerId', axis=1)
+
+features = int(len(x_train.columns))
+print('features:', features)
+
+log_reg = LogisticRegression()
+log_reg.fit(x_train, y_train)
+
+y_predict = log_reg.predict(x_test)
+
+acc_log = round(log_reg.score(x_train, y_train) * 100, 2)
+
+print('Logistic regression accuracy:', acc_log)
+
+svc = SVC()
+svc.fit(x_train, y_train)
+y_predict = svc.predict(x_test)
+acc_svc = round(svc.score(x_train, y_train)*100, 2)
+print('SVM regression accuracy:', acc_svc)
+
+
+#tensorflow
+
+
+b = tf.Variable(tf.zeros([1, 1]))
+x = tf.placeholder(tf.float32, [None, features])
+
+w = tf.Variable(tf.zeros([features, 1]))
+
+#output
+z = tf.matmul(x, w) + b
+y = tf.nn.sigmoid(z)
+
+y_ = tf.placeholder(tf.float32, [None, 1])
+
+lbda = tf.constant(0.000)
+
+loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=y_, logits=z) + lbda * tf.nn.l2_loss(w))
+
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+sess = tf.InteractiveSession()
+init = tf.global_variables_initializer()
+sess.run(init)
+
+y_train = y_train.values.reshape(-1, 1)
+x_train = x_train.values
+
+epoch = 100000
+for index in range(epoch):
+    _ = sess.run([train_step], feed_dict={x: x_train, y_: y_train})
+    if index % 100 == 0:
+        loss_value = sess.run([loss], feed_dict={x: x_train, y_: y_train})
+        print('loss:', loss_value)
+
