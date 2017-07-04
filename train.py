f8d25d33e10bcc8f5bcb1f5ea030abd0c3ab706a
Jackie Zhang <viperking@163.com>
2017-07-04 23:15:01 +0800

    refactored code added accuracy eval

--- train.py
+++ train.py
@@ -46,6 +46,8 @@ print('SVM regression accuracy:', acc_svc)
 
 #tensorflow
 
+y_train = y_train.values.reshape(-1, 1)
+x_train = x_train.values
 
 b = tf.Variable(tf.zeros([1, 1]))
 x = tf.placeholder(tf.float32, [None, features])
@@ -66,21 +68,19 @@ loss = -tf.reduce_mean(y_ * tf.log(y) + (1 - y_) * tf.log(1 - y)) + lbda * tf.nn
 
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
 
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+correct_prediction = tf.less(tf.abs(y - y_), 0.5)
 
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-sess = tf.InteractiveSession()
-init = tf.global_variables_initializer()
-sess.run(init)
+with tf.Session() as sess:
+    init = tf.global_variables_initializer()
+    sess.run(init)
 
-y_train = y_train.values.reshape(-1, 1)
-x_train = x_train.values
-
-epoch = 100000
-for index in range(epoch):
-    _ = sess.run([train_step], feed_dict={x: x_train, y_: y_train})
-    if index % 100 == 0:
-        loss_value = sess.run([loss], feed_dict={x: x_train, y_: y_train})
-        print('loss:', loss_value)
-
+    epoch = 10000
+    for index in range(epoch):
+        _ = sess.run([train_step], feed_dict={x: x_train, y_: y_train})
+        if index % 100 == 0:
+            loss_value = sess.run([loss], feed_dict={x: x_train, y_: y_train})
+            print('loss:', loss_value)
+            acc_value = accuracy.eval(feed_dict={x: x_train, y_: y_train})
+            print('accuracy:', acc_value)
